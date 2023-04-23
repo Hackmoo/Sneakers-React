3d0f837a8707957a496704e505e48ea4591cde9e
Hackmoo <hackmo872@gmail.com>
2023-04-23 17:18:33 +0300

    goodCard has been fixed

--- sneakers/src/components/goodsList/Good.jsx
+++ sneakers/src/components/goodsList/Good.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import './good.scss'
 import Button from '@mui/material/Button'
+import DoneIcon from '@mui/icons-material/Done';
+import AddIcon from '@mui/icons-material/Add';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { SvgIcon } from "@mui/material";
 
 function Good({imgPath = './img/nike-red.png', man = true, name = 'Nike Air Max 180', price = '500'}){
     return (
         <div className="card">
+            <SvgIcon style={{padding: '5px 0 0 10px'}}><FavoriteBorderIcon/></SvgIcon>
             <img src={imgPath} alt="alt" width={'230px'} height={'220px'}/>
             {man ? (<span>Men's sneakers: <br/>{name}</span>) : (<p>Women's sneakers: <br/>{name}</p>)}
             <div className="text">
               <span className="underGood">Price:  <strong>{price}$</strong></span>
-              <Button variant="contained" size="small">Add</Button>
+              <Button variant="contained" size="small"><AddIcon/></Button>
             </div>
         </div>
     )
--- sneakers/src/components/goodsList/GoodsList.jsx
+++ sneakers/src/components/goodsList/GoodsList.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Good from './Good'
 import './goodList.scss'
+import SearchIcon from '@mui/icons-material/Search';
+import { SvgIcon } from "@mui/material";
 
 
 export function GoodsList(){
     return(
         <div className="wrapperGoods">
-            <h2>All Sneakers</h2>
+            <div className="searcher">
+            <h1>All Sneakers</h1>
+             <div className="search-block">
+              <SvgIcon fontSize="medium" className="search-icon"><SearchIcon/></SvgIcon>
+              <input type="text" placeholder="Search..." />
+             </div>
+            </div>
             <div className="goods">
-             <Good />
-             <Good />
-             <Good />
-             <Good />
              <Good />
              <Good />
              <Good />
